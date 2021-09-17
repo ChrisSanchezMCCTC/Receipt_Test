d0b039482ba4520507136f76b0a1430e99b1dc7c
Chris Sanchez <sanchez_christopher@student.mahoningctc.com>
2021-09-17 07:58:31 -0400

    Notes

---
+++ Control Flow.py
@@ -0,0 +1,44 @@
+Pick_a_number = input("pick a number: ")
+
+
+
+def character_name():
+    char_name = input("Please enter your name: ")
+    print(f"Hello {char_name}. Welocme to the game.")
+
+if(Pick_a_number == "5"):
+    character_name()
+else:
+    print("You did not pick the right number.")    
+
+
+
+character_name()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
